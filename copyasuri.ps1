191b539f27cc99a0f001c9be4558b6549849bbad
yunusyurtturk <yunusyurtturk@gmail.com>
2024-11-22 12:04:30 +0300

    Added checks and improved tests

--- copyasuri.ps1
+++ copyasuri.ps1
@@ -1,12 +1,19 @@
 ﻿$RegistryPath = "HKCU:\Software\Classes\*\shell\Copy as URI"
-$RegistryValue = "Copy path as URI" # Menu entry that show up in context menu
-
-New-Item -Path $RegistryPath.Replace('`', '') -Force
-Set-ItemProperty -Path $RegistryPath -Name "(Default)" -Value $RegistryValue
-
 $RegistryCommandPath = "$RegistryPath\command"
-$RegistryCommand = "powershell.exe -NoProfile -Command `"Add-Type -AssemblyName PresentationCore; Add-Type -AssemblyName PresentationFramework; [System.Windows.Clipboard]::SetText('file:///' + ('%1' -replace '\\', '/'))`""
+$RegistryValue = "Copy path as URI" # Displayed text in the context menu
+$RegistryCommand = @"
+powershell.exe -NoProfile -Command "Add-Type -AssemblyName PresentationCore; Add-Type -AssemblyName PresentationFramework; [System.Windows.Clipboard]::SetText('file:///' + ('%1' -replace '\\', '/'))"
+"@
 
-New-Item -Path $RegistryCommandPath -Force | Out-Null
+if (-not (Test-Path -Path $RegistryPath)) {
+    New-Item -Path $RegistryPath -Force | Out-Null
+}
 
-New-ItemProperty -Path $RegistryCommandPath -Name "(Default)" -Value $RegistryCommand -Force | Out-Null
+Set-ItemProperty -Path $RegistryPath -Name "(Default)" -Value $RegistryValue -Force
+
+if (-not (Test-Path -Path $RegistryCommandPath)) {
+    New-Item -Path $RegistryCommandPath -Force | Out-Null
+}
+
+Set-ItemProperty -Path $RegistryCommandPath -Name "(Default)" -Value $RegistryCommand -Force | Out-Null
+Write-Host "Context menu 'Copy as URI' added successfully." -ForegroundColor Green
